bfe3272b1f927836b84ce7ae142053c800c87c2f
KataRiegels <62016188+KataRiegels@users.noreply.github.com>
2022-06-05 18:14:54 +0200

    Item clean + comments

--- Expired/Items/Item.py
+++ Expired/Items/Item.py
@@ -1,27 +1,30 @@
 from widgets.item_display import FoodItemSelection
-from . import Date
-from widgets import *
+from .                    import Date
+from random               import randint
 
-from random import randint
+
+"""
+Food item class
+"""
 class Item():
     
-    def __init__(self,product_name = "NaN",expiry_date=Date(2000,1,1), ID = "Invalid"):
-        self.product_name = product_name
-        self.expiry_date  = expiry_date
-        self.ID          = ID
+    def __init__(self, product_name = "NaN",expiry_date=Date(2000,1,1), ID = "Invalid"):
+        self.product_name        = product_name
+        self.expiry_date         = expiry_date
+        self.ID                  = ID
         self.food_item_selection = FoodItemSelection(_owner = self)
         self.food_item_selection.createOption()
 
     """ Generates an ID for an item with 4 random digits"""
     def create_unique_ID(self):
-        ID      = str(randint(1000,4999))
-        self.ID = self.product_name + ID
+        ID_number  = str(randint(1000,4999))
+        self.ID    = self.product_name + ID_number
 
     """ Arranges the information so it can be stored in the JSON file """
     def as_JSON_format(self):
         return [self.product_name,[self.expiry_date.year,self.expiry_date.month,self.expiry_date.day]]
 
-    """ In order for displaying the item """
+    """ In order to display the item text """
     def as_string(self):
         return f"{self.product_name} expiring {self.expiry_date.as_string()}"
 
