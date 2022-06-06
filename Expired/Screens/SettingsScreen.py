17ccc13210e867b4d2bc7a7d755e1041b502f1ed
KataRiegels <62016188+KataRiegels@users.noreply.github.com>
2022-06-06 11:35:51 +0200

    settingsscreen.py partly clean + partly comments

--- Expired/Screens/SettingsScreen.py
+++ Expired/Screens/SettingsScreen.py
@@ -1,61 +1,62 @@
-from kivy.uix.screenmanager import Screen
-from . import MyScreen
-from kivy.utils import platform
+from kivy.utils              import platform
 from kivy.network.urlrequest import UrlRequest
-from kivy.uix.button import Button
-from kivymd.uix.button import MDRaisedButton
-from kivymd.uix.button import MDFillRoundFlatButton
-from kivymd.uix.picker import MDThemePicker
-from kivymd.uix.label import MDLabel
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.app import MDApp
+from kivymd.uix.button       import MDFillRoundFlatButton
+from kivymd.uix.picker       import MDThemePicker
+from kivymd.app              import MDApp
+from .                       import MyScreen
 import csv
 
+"""
+Settings screen class
+"""
 class SettingsScreen(MyScreen):
-
     
+    """ For making API test buttons appear """
     def enable_features(self):
         self.ids.features
+        # If test feeature are NOT enabled
         if self.ids.features.text == "Enable test features":
             self.ids.label_text.text = "API response" 
-            self.ids.features.text = "Disable test features"
-            self.api2 = MDFillRoundFlatButton(text= 'test cat api',
-                    size_hint=(.5,.1),
-                    pos_hint= {'x':.25,'y':.4},
-                    on_press= self.apisaved_date)
+            self.ids.features.text   = "Disable test features"
+            self.api2 = MDFillRoundFlatButton(text= 'Test cat fact API',
+                    size_hint = (.5,.1),
+                    pos_hint  = {'x':.25,'y':.4},
+                    on_press  = self.apisaved_date)
             self.add_widget(self.api2)
-            self.api1 = MDFillRoundFlatButton(text= 'test cat api (PC)',
-                    size_hint=(.5,.1),
-                    pos_hint= {'x':.25,'y':.3},
-                    on_press= self.apiTEST1)
+            self.api1 = MDFillRoundFlatButton(text= 'Test cat fact API (PC)',
+                    size_hint = (.5,.1),
+                    pos_hint  = {'x':.25,'y':.3},
+                    on_press  = self.apiTEST1)
             self.add_widget(self.api1)
+        # If test feeature ARE enabled
         elif self.ids.features.text == "Disable test features":
             self.ids.label_text.text = "" 
-            self.ids.features.text = "Enable test features"
+            self.ids.features.text   = "Enable test features"
             self.remove_widget(self.api1)
             self.remove_widget(self.api2)
-            
+    
+    """ For opening the theme picker widget """
     def show_theme_picker(self):
         theme_dialog = MDThemePicker(on_pre_dismiss = self.save_theme)
         theme_dialog.open()  
-        print(f"Theme is: {MDApp.get_running_app().theme_cls.primary_palette}")
-            
+    
+    """ Saving picked theme to theme.csv file so it is remembered """
     def save_theme(self,instance):
         app_theme = MDApp.get_running_app().theme_cls
         with open('save_files/theme.csv', 'w') as f:
             writer = csv.writer(f)
             writer.writerow([app_theme.primary_palette,app_theme.theme_style])
-        pass
-            
+    
+    """ Requesting cat fact API response """        
     def apisaved_date(self,instances):
-        request = UrlRequest("https://catfact.ninja/fact", on_success=self.get_data)
+        UrlRequest("https://catfact.ninja/fact", on_success=self.get_data)
 
+    """ Displaying cat fact """
     def get_data(self,request,response):
         self.ids.label_text.text = response['fact']
     
     def apiTEST1(self):
         pass
-    pass
 """
 if platform == ('win'):
     from textractcaller import call_textract
